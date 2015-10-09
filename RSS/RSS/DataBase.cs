598e062f0b68aa27f965a9798ca6ba69454011ba
Justa3122 <justyna_150393@poczta.onet.pl>
2015-10-09 23:22:47 +0200

    dodanie klasy News, DataBase

---
+++ RSS/RSS/DataBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS
+{
+    class DataBase
+    {
+        void AddNews()//zapisywanie komunikatow tych, ktorych jeszcze nie ma (sprawdzanie po ID)
+        {
+        }
+        void DeleteNews()//zostawiamy 10 najnowszych komunikatow, reszte usuwamy
+        {
+        }
+    }
+}
---
+++ RSS/RSS/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS
+{
+    class News
+    {
+        public int IdNews { get; set; }
+        public string TitleOfNews { get; set; }
+        public DateTime DateAddNews { get; set; }
+        public string Message { get; set; }
+    }
+}
