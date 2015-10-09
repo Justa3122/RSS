6f30281de14ca02c28602af1ea7b25374b7809b9
Zealur <zealur@interia.pl>
2015-10-09 23:30:33 +0200

    Dodanie elementów klasy RSS

--- RSS/RSS/News.cs
+++ RSS/RSS/News.cs
@@ -8,9 +8,12 @@ namespace RSS
 {
     class News
     {
+        //Adres RSS: http://rss.wp.pl/s.kina.index.html
+
         public int IdNews { get; set; }
         public string TitleOfNews { get; set; }
         public DateTime DateAddNews { get; set; }
         public string Message { get; set; }
+        public string NameOfChannel { get; set; }
     }
 }
