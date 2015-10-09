ff79a5f08fd21983064993e72ece694862886c69
Zealur <zealur@interia.pl>
2015-10-09 22:39:12 +0200

    Dodanie klasy RSSReader

---
+++ RSS/RSS/RSSReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS
+{
+    class RSSReader
+    {
+        public int id;
+        public string message;
+    }
+}
