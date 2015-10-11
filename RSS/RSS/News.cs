f6dc44e7532aeac089698479a35af4574eccaeb0
Zealur <zealur@interia.pl>
2015-10-11 20:17:26 +0200

    Dodanie funkcji w RssReader, rozbudowa klasy News, Dodanie klasy Description

--- RSS/RSS/News.cs
+++ RSS/RSS/News.cs
@@ -8,12 +8,21 @@ namespace RSS
 {
     class News
     {
-        //Adres RSS: http://rss.wp.pl/s.kina.index.html
+        //Adres RSS: http://rss.wp.pl/s,kina,index.html
 
-        public int IdNews { get; set; }
         public string TitleOfNews { get; set; }
-        public DateTime DateAddNews { get; set; }
-        public string Message { get; set; }
-        public string NameOfChannel { get; set; }
+        public string Category { get; set; }
+        public string LinkToWebsite { get; set; }
+        public DateTime DateOfPublication { get; set; }
+        public Description DescriptionOfNews { get; set; }
+
+        public News(string title, string description, string link, string category, string pubdate)
+        {
+            this.TitleOfNews = title;
+            this.LinkToWebsite = link;
+            this.Category = category;
+            this.DateOfPublication = DateTime.Parse(pubdate);
+            this.DescriptionOfNews = new Description(description);
+        }
     }
 }
--- RSS/RSS/RSSReader.cs
+++ RSS/RSS/RSSReader.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace RSS
 {
-    class RSSReader
+    static class RSSReader
     {
+        static void ReadNews(string channel)
+        {
+            XmlReader reader = XmlReader.Create(channel);
+            News news;
+            string title = "",
+                link = "",
+                category = "",
+                pubdate = "",
+                description = "";
+
+            while (reader.Read())
+            {
+                if (reader.IsStartElement())
+                {
+                    switch (reader.Name)
+                    {
+                        case "title":
+                            reader.Read();
+                            title = reader.Value.Trim();
+                            break;
+                        case "link":
+                            reader.Read();
+                            link = reader.Value.Trim();
+                            break;
+                        case "category":
+                            reader.Read();
+                            category = reader.Value.Trim();
+                            break;
+                        case "description":
+                            reader.Read();
+                            description = reader.Value.Trim();
+                            break;
+                        case "pubDate":
+                            reader.Read();
+                            pubdate = reader.Value.Trim();
+                            news = new News(title, description, link, category, pubdate);
+                            //Dopiero po wczytaniu elementu <pubDate> zostaje stworzony obiekt klasy,
+                            //ponieważ ten element jest zawsze ostatnim elementem wiadomości
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
