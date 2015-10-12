7294d2f4e9e73baee40a94be1d5fa5828a515b29
despo21 <szlodziana@gmail.com>
2015-10-13 00:45:27 +0200

    cd

--- RSS/RSS/News.cs
+++ RSS/RSS/News.cs
@@ -3,30 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity;
 
 namespace RSS
 {
     public class News
     {
         //Adres RSS: http://rss.wp.pl/s,kina,index.html
-        public int NewsId { get; private set; }
+
+        public int NewsID { get; private set; }
         public string TitleOfNews { get; set; }
-        public string Category { get; set; }
         public string LinkToWebsite { get; set; }
         public DateTime DateOfPublication { get; set; }
         public Description DescriptionOfNews { get; set; }
 
-        public News()
+        public News(int newsId, string title, string description, string link, string pubdate)
         {
-
-        }
-        public News(int newsId, string title, string description, string link, string category, string pubdate)
-        {
-            this.NewsId = newsId;
+            this.NewsID = newsId;
             this.TitleOfNews = title;
             this.LinkToWebsite = link;
-            this.Category = category;
             this.DateOfPublication = DateTime.Parse(pubdate);
             this.DescriptionOfNews = new Description(description);
         }
