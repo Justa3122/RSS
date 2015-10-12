a725346b7bd9fa544131d188d2b592e245e54f52
despo21 <szlodziana@gmail.com>
2015-10-13 00:46:03 +0200

    cd

--- RSS/RSS/MainWindow.xaml.cs
+++ RSS/RSS/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace RSS
             System.Windows.Media.Brush col1 = OuterGlow.BorderBrush;
             DropShadowEffect.Color = ((SolidColorBrush)col1).Color;
             DataBase db = new DataBase();
-            News wojna = new News(1, "wojna w rosji", "fsdfs", "dada", "sdasda", "sd");
+            News wojna = new News(1, "wojna w rosji", "sdasd", "sdasd", "sdas");
             Category swiat = new Category(1, "swiat");
             swiat.AddNews(wojna);
             db.Categories.Add(swiat);
@@ -41,6 +41,13 @@ namespace RSS
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Application.Current.MainWindow.Close();
+        }
+        private void Rectangle_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
     }
 }
