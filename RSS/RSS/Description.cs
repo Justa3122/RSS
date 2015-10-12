ab190f2a19c19b6402154e4a9daa1befa9f40d22
Zealur <zealur@interia.pl>
2015-10-12 14:58:09 +0200

    Dodanie Resources, dodanie klasy Description

---
+++ RSS/RSS/Description.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS
+{
+    /// <summary>
+    /// Klasa służy do odczytu znacznika <description>, ponieważ zawiera ona: opis, obrazek oraz link
+    /// przez co XmlReader sobie z tym nie radzi.
+    /// </summary>
+    class Description
+    {
+        public string Text { get; set; }
+        public string ImgsrcFragment { get; set; }
+        public string ImageLink { get; set; }
+
+        private void GetImageLink(string imgsrcFragment)
+        {
+            ImageLink = imgsrcFragment.Substring(imgsrcFragment.IndexOf("src=") + 5, (imgsrcFragment.IndexOf("width") - 2) - (imgsrcFragment.IndexOf("src=") + 5));
+        }
+        public Description(string description)
+        {
+            try
+            {
+                ImgsrcFragment = description.Substring(description.IndexOf("<img"), description.IndexOf("/>") + 2);
+                Text = description.Substring(description.IndexOf("/>") + 2, description.IndexOf("<a") - (description.IndexOf("/>") + 2));
+                GetImageLink(ImgsrcFragment);
+                //Do poprawy ~KG > Zbudowane pod kanał: "Wiadomości"
+                //Na kanale z wiadomościami, ostatni news ma inna budowę przez co opis zawiera linka.
+            }
+            catch
+            {
+                Text = description;
+                ImgsrcFragment = null;
+                ImageLink = null;
+            }
+        }
+    }
+}
