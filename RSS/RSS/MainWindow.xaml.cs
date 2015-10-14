8f47161742e26531f7a3d75e6a499086bc2d7bb4
Justa3122 <justyna_150393@poczta.onet.pl>
2015-10-14 09:47:38 +0200

    wygląd początkowy

--- RSS/RSS/MainWindow.xaml.cs
+++ RSS/RSS/MainWindow.xaml.cs
@@ -22,15 +22,43 @@ namespace RSS
     {
         public MainWindow()
         {
+            
             InitializeComponent();
+            AppDomain.CurrentDomain.SetData("DataDirectory", AppDomain.CurrentDomain.BaseDirectory);
             System.Windows.Media.Brush col1 = OuterGlow.BorderBrush;
             DropShadowEffect.Color = ((SolidColorBrush)col1).Color;
-            DataBase db = new DataBase();
+            //DataBase db = new DataBase();
             News wojna = new News(1, "wojna w rosji","<img src=/>- Jestem gotów poprzeć rząd mniejszościowy PiS, żeby nie destabilizować państwa - mówi w Kontrwywiadzie RMF FM szef ruchu Kukiz&#8217;15 Paweł Kukiz. - Chcę być potężnym językiem u wagi, który zmieni ustrój Polski, zmieni konstytucję, doprowadzi do tego, że Sejm zacznie pracować. Nie chcę rządzić po tych wyborach. Ja się nie nadaję na wicepremiera - dodaje. - Sondaże? Jakie sondaże, proszę pana. Patrzę na wyniki Petru i jestem przekonany, że on jest symulowany przez te sondaże - komentuje Kukiz. <a href=>wiadomosci.wp.pl</a>","dsadsa", "Mon, 12 Oct 2015 08:45:00 +0200");
             Category swiat = new Category(1, "swiat");
             swiat.AddNews(wojna);
-            db.Categories.Add(swiat);
-            db.SaveChanges();
+            string link1 = "http://i.wp.pl/a/f/film/001/12/12/0431212.jpg";
+            string link2 = "http://i.wp.pl/a/f/film/001/84/76/0437684.jpg";
+            string link3 = "http://i.wp.pl/a/f/film/001/10/34/0433410.jpg";
+            string link4 = "http://i.wp.pl/a/f/film/001/76/45/0434576.jpg";
+            string link5 = "http://i.wp.pl/a/f/film/001/23/35/0433523.jpg";
+            image1.Source = new BitmapImage(new Uri(link1));
+            image2.Source = new BitmapImage(new Uri(link2));
+            image3.Source = new BitmapImage(new Uri(link3));
+            image4.Source = new BitmapImage(new Uri(link4));
+            image5.Source = new BitmapImage(new Uri(link5));
+            expander.Content = swiat.Name;
+            TextBlock1.Text = wojna.DescriptionOfNews.Text;
+            TextBlock2.Text = "Fajne się wydaje";
+            TextBlock3.Text = "Tyty ryty trytytki!";
+            TextBlock4.Text = "Chcę oglądać! ";
+            TextBlock5.Text = "To też chcę oglądać :) ";
+            //if (label1.MouseDoubleClick())
+            //{
+            //    TextBlock4.Text = "KLIK ";
+            //}
+           
+            
+
+
+
+
+            //db.Categories.Add(swiat);
+            //db.SaveChanges();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
